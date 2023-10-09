04efce9b47130614f828dacff49c4e262fed168a
noslopy <info@noslopy.dev>
2023-10-09 16:45:20 +0200

    Remove landing page template

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        This will be the company HQ
       </div>
-      <h1>Vite + React</h1>
+      <h1>Birdhouse</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is where some text goes
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Terms and Conditions
       </p>
     </>
   )
