f4b213d7e06fbe61b08da9b6065b9a6865b0eb88
noslopy <info@noslopy.dev>
2023-10-29 10:41:28 +0100

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import './App.css'
+import { Admin, Resource, ListGuesser} from 'react-admin';
+import { dataProvider } from './dataProvider';
+import { UserList } from "./users";
 
 function App() {
 
   return (
     <>
-      <div>
-        This will be the company HQ
-      </div>
-      <h1>Birdhouse</h1>
-      <div className="card">
-        <p>
-          This is where some text goes
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Terms and Conditions
-      </p>
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={UserList} />
+        <Resource name="posts" list={ListGuesser} />
+      </Admin>
     </>
   )
 }
